7d042d4fec15baf33e648262a66aef78cfe07efc
Daoming Liu <ldm924940564@icloud.com>
2025-04-02 11:58:39 -0700

    removed additional attention mask

--- contrastive_encoders/encoders.py
+++ contrastive_encoders/encoders.py
@@ -12,11 +12,11 @@ from transformers.models.qwen2_5_vl.modular_qwen2_5_vl import Qwen2_5_VLVisionCo
 
 import math
 
-def MeanPooler(hidden_states, attention_mask = False):
+def MeanPooler(hidden_states, attention_mask = None):
     if attention_mask is not None:
-        pooled_output = torch.sum(hidden_states*attention_mask.unsqueeze(-1), dim=0) / torch.sum(attention_mask, dim=-1).unsqueeze(-1)
+        pooled_output = torch.sum(hidden_states*attention_mask.unsqueeze(-1), dim=1) / torch.sum(attention_mask, dim=-1).unsqueeze(-1)
     else:
-        pooled_output = torch.mean(hidden_states, dim=0, keepdim=True)
+        pooled_output = torch.mean(hidden_states, dim=1, keepdim=True)
     return pooled_output
 
 
@@ -30,17 +30,16 @@ class VisionEncoder(Qwen2_5_VLPreTrainedModel):
         self.encoder = Qwen2_5_VisionTransformerPretrainedModel(vision_config)
         print(f"V Encoder config: {self.encoder.config}")
         self.pooler = MeanPooler
-        # Add a pooling layer
     
 
 
-    def forward(self, input_id, pixel_values, grid_thw, attention_mask: Optional[torch.Tensor] = None):
+    def forward(self, input_ids, pixel_values, grid_thw, attention_mask: Optional[torch.Tensor] = None):
+        batch_size = input_ids.shape[0]
         seq_len = self.encoder.config.seq_len
         out_hidden_size = self.encoder.config.seq_len
         
-        hidden_states = self.encoder(hidden_states=pixel_values, grid_thw=grid_thw) # (num_tokens, out_hidden_size)
+        video_embeds = self.encoder(hidden_states=pixel_values, grid_thw=grid_thw) # (num_tokens, out_hidden_size)
 
-        input_ids = inputs['input_ids']
         n_video_tokens = (input_ids == config.video_token_id).sum().item()
         n_video_features = video_embeds.shape[0]
         if n_video_tokens != n_video_features:
@@ -48,35 +47,35 @@ class VisionEncoder(Qwen2_5_VLPreTrainedModel):
                 f"Video features and video tokens do not match: tokens: {n_video_tokens}, features {n_video_features}"
             )
             
-        psudo_input_embdeds = torch.zeros(1, seq_len, out_hidden_size)
+        pseudo_input_embdeds = torch.zeros(batch_size, seq_len, out_hidden_size)
         video_mask = (
             (input_ids == config.video_token_id)
             .unsqueeze(-1)
-            .expand_as(psudo_input_embdeds)
-            .to(psudo_input_embdeds.device)
+            .expand_as(pseudo_input_embdeds)
+            .to(pseudo_input_embdeds.device)
         )
         
-        video_embeds = video_embeds.to(psudo_input_embdeds.device, psudo_input_embdeds.dtype)
+        video_embeds = video_embeds.to(pseudo_input_embdeds.device, pseudo_input_embdeds.dtype)
         
-        batched_hidden_states = psudo_input_embdeds.masked_scatter(video_mask, video_embeds)
+        batched_hidden_states = pseudo_input_embdeds.masked_scatter(video_mask, video_embeds)
 
-        attention_mask = torch.zeros(batch_size, seq_len)
-
-        if batch_size == 1:
-            attention_mask[0, 15:-7] = 1
-        elif batch_size == 2:
-            attention_mask[0, 15:] = 1
-            attention_mask[1, 7:-7] = 1
-        else:
-            attention_mask[0, 15:] = 1
-            attention_mask[1:-1, 7:] = 1
-            attention_mask[-1, 7:-7] = 1
+        # if batch_size == 1:
+        #     attention_mask[0, 15:-7] = 1
+        # elif batch_size == 2:
+        #     attention_mask[0, 15:] = 1
+        #     attention_mask[1, 7:-7] = 1
+        # else:
+        #     attention_mask[0, 15:] = 1
+        #     attention_mask[1:-1, 7:] = 1
+        #     attention_mask[-1, 7:-7] = 1
 
         # expanded_mask = attention_mask.unsqueeze(-1).expand_as(batched_hidden_states)
 
         # masked_hidden_states = batched_hidden_states * expanded_mask
         
-        pooled_output = self.pooler(batched_hidden_states,attention_mask) 
+        pooled_output = self.pooler(batched_hidden_states,
+                                    attention_mask=video_mask
+                                    ) 
         
         return pooled_output
 
