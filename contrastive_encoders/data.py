4f4aa572feab837d81e551c948dc28c2e2a2ff0a
Ethan Wong <ethanwongca@gmail.com>
2025-04-04 11:25:46 -0700

    Updated to handle right padidng

--- contrastive_encoders/data.py
+++ contrastive_encoders/data.py
@@ -2,12 +2,12 @@ from typing import Any, Dict, List
 from lightning.pytorch.core import LightningDataModule
 from torch.utils.data import DataLoader
 import webdataset as wds
-from transformers import AutoImageProcessor
+import torch
+from transformers import AutoProcessor
 import os 
 
 # Modified Qwen Processor Class
 from utils import vision_utils
-
 
 class DataCollatorWithPadding:
     def __init__(self, processor: Any, data_dir: str, stage:str, max_length: int = 128) -> None:
@@ -18,23 +18,47 @@ class DataCollatorWithPadding:
 
     def __call__(self, batch: List[Dict[str, Any]]) -> Dict[str, Any]:
         # Processing videos via Qwen's Image Processor
- 
         # Extra the bytes from the webdataset as mp4 -> bytes 
         video_data_list = [sample[key] for sample in batch for key in sample if key.endswith("mp4")]
-
         # Process the raw video data via modified Qwen-VL Utils for the webdataset 
-        video_inputs = vision_utils.process_vision_info(video_data_list)
+        video_inputs, video_kwargs = vision_utils.process_vision_info(video_data_list, return_video_kwargs=True)
+
+        # Prompt needs the EOS Tokens Etc. 
+        messages = [{"role": "user", "content": [{"type": "video"}]}]
+        prompt = self.processor.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+
         processed_videos = self.processor(
+            text=[prompt] * len(video_data_list),
             images=None,  # Only processing videos here.
             videos=video_inputs,
+            padding=True, 
             return_tensors="pt",
+            **video_kwargs,
         )
+        
+        # Left padding so we index end of attention mask and extend to the end 
+        # Both input_id: (batch_size, sequence_length) and attention_mask: (batch_size, sequence_length)
+        num_examples = processed_videos['attention_mask'].shape[0]
 
+        # Set right padding since array is contiguous 
+        for i in range(num_examples):
+            input_id = processed_videos['input_ids'][i]
+            attention_mask = processed_videos['attention_mask'][i]
+        
+            padding = input_id[attention_mask == 0]
+            non_padding = input_id[attention_mask == 1]
+        
+            new_input_id = torch.cat([non_padding, padding], dim=0)
+            new_attention_mask = torch.cat([torch.ones_like(non_padding), torch.zeros_like(padding)], dim=0)
+        
+            processed_videos['input_ids'][i] = new_input_id
+            processed_videos['attention_mask'][i] = new_attention_mask
+            
         texts = [sample[key] for sample in batch for key in sample if key.endswith("txt")]
-
+        
         return {
             "videos": processed_videos,
-            "texts": texts,
+            "texts": texts
         }
 
 class DataModule(LightningDataModule):
@@ -43,7 +67,7 @@ class DataModule(LightningDataModule):
         self.data_dir = data_dir
         self.cfg = cfg
         
-        self.processor = AutoImageProcessor.from_pretrained("Qwen/Qwen2.5-VL-3B-Instruct")
+        self.processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-3B-Instruct")
         
         self.collator = DataCollatorWithPadding(
             processor=self.processor,
@@ -54,7 +78,7 @@ class DataModule(LightningDataModule):
 
     def setup(self, stage: str = None) -> None:
         if stage in ("fit", None):
-            train_path = os.path.join(self.data_dir, "train",  "{00000..00089}.tar")
+            train_path = os.path.join(self.data_dir, "train",  "{00000..00031}.tar")
             self.train_dataset = wds.WebDataset(train_path).shuffle(1000)
         if stage in ("validate", None):
             val_path = os.path.join(self.data_dir, "val",  "{00000..00010}.tar")
@@ -86,7 +110,6 @@ class DataModule(LightningDataModule):
             num_workers=self.cfg["num_workers"],
             collate_fn=self.collator
         )
-
 
 if __name__ == "__main__":
     # Example cfg input and data_dir
